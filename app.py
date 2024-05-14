91d43ba0c602155a37725ae1c24f722c4decf8e6
quanba3008 <113974246+quanba3008@users.noreply.github.com>
2024-05-15 00:11:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ def api_delete(id):
     return "Success: Book information has been deleted."
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
